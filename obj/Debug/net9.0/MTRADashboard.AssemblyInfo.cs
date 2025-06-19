cb9c339e5c134cb78a649efc3e43d8d114fa8539
Aariz18 <mohd.aariz13518@gmail.com>
2025-06-19 13:08:34 +0400

    idk what im doing

--- obj/Debug/net9.0/MTRADashboard.AssemblyInfo.cs
+++ obj/Debug/net9.0/MTRADashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTRADashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26ee76b993bf96119a0c834df7920da7a0c0db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTRADashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTRADashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
