7a6daacf96bfe1da7e7042a0da29ee79c534e303
Aariz18 <mohd.aariz13518@gmail.com>
2025-06-25 10:30:21 +0400

    Adjusted the frontend of the login page

--- obj/Debug/net9.0/MTRADashboard.AssemblyInfo.cs
+++ obj/Debug/net9.0/MTRADashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTRADashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f81002c1573ac392075eb4fa7fcbb95fa850290")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+573452b671b4d4864129d99d0ecebac3da5f7c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTRADashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTRADashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
